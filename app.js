155006c20f1cbc5307404fc1f178da3a7a365d19
Sailaja <sailaja.g1997@gmail.com>
2019-10-02 19:50:04 +0530

    FIRST COMMIT

--- app.js
+++ app.js
@@ -1,1 +1,90 @@
-alert `test`;
+
+//var str = "Hi";
+//var str1 = 'Hello';
+//var str2 = `How are you`;//Template String(ES-6 onwards)
+//var str3 =`
+//<h1>Hello</h1>
+//<p>Lorem Is ............</p>
+//<button>Clickme</button>
+//<input type="text" placehloder="Enter text"/>
+//${5+5}`;
+
+
+//document.write(str3);
+
+//Secoond EX:
+//var name="Nani";
+//var age =20;
+//var company ="Infosys";
+
+//document.write (`My name is ${name} and My age is ${age} and I worked for ${company}` )  ;
+
+//How to check Datatype:
+//var name ="Nani";
+//var age =20;
+
+//console.log (typeof name);
+//console.log (typeof age);
+
+//Number Data type:
+//var num=1;
+//var num1=10.90;
+//var num2=1/1;
+
+//console.log (typeof num);
+//console.log (typeof num1);
+//console.log (typeof num2);
+
+//NaN :Not a Number:
+//var name="Ani"/0;
+
+//console.log (typeof name);
+
+//For looping
+//for (var i = 0 ; i < 10 ; i++)
+//{
+       // var name = "Sachin";//Global Scope
+        //console.log(i);
+//}
+   // console.log(name);
+
+for (let i = 0 ; i < 10 ; i++)
+{
+        let name ="nani";//Local Scope
+        console.log(i)
+}        
+    console.log(name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
